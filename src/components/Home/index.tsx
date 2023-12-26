1a315868d2a0e498c8a60b4d17910b2f9474a135
Akshat-Dwivedi <akshat10dwivedi@gmail.com>
2023-12-26 16:49:10 +0530

    general classes

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,12 @@
+import cn from "classnames";
+
 import styles from "./index.module.scss";
+import "@styles/_common.scss";
 
 const HomePage = () => {
   return (
     <div>
-      <div className={styles.home}>Home Page</div>
+      <div className={cn(styles.home, "column p-s")}>Home Page</div>
     </div>
   );
 };
